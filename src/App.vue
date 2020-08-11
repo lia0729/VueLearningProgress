a9ca3e2d84b044c79da5bea0712e791af6c27354
bowenlu <bowenlu729@gmail.com>
2020-08-11 16:32:50 +0800

    single page vue test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/sep11.jpeg" width="500">
+    <HelloWorld msg="LIA 的 青春"/>
   </div>
 </template>
 
